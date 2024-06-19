e2792d78f31586f06892a8b00479a9d246a0fe3b
innovatorbize <innovatorbize@gmail.com>
2024-06-19 21:47:05 +0530

    19-06-2024.   - Demo Components Updated

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -3,14 +3,19 @@ import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from "@angular/common";
 import { CustomNumberboxComponent } from '../../components/custom-numberbox/custom-numberbox.component';
 import { CustomSelectBoxComponent } from '../../components/custom-selectbox/custom-selectbox.component';
+import { CustomTextboxComponent } from '../../components/custom-textbox/custom-textbox.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   standalone : true,
-  imports : [FormsModule, CustomNumberboxComponent, CustomSelectBoxComponent]
+  imports : [FormsModule, CustomNumberboxComponent, CustomSelectBoxComponent, CustomTextboxComponent]
 })
 export class LoginComponent {
-
+  roles : any = [
+    { key: 'admin', value: 'Administrator' },
+    { key: 'user', value: 'User' },
+    { key: 'guest', value: 'Guest' }
+  ]
 }
